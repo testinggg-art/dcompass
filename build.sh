b2cf80c7422bdb7472e6b8b384ce98ea7e152eb3
Harry Ying <lexugeyky@outlook.com>
2020-11-20 21:01:38 +0800

    build: new building system

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+targetList=(
+	"x86_64-unknown-linux-musl"
+	"armv7-unknown-linux-muslabi"
+	"armv5te-unknown-linux-musleabi"
+	"x86_64-pc-windows-gnu"
+	"x86_64-apple-darwin"
+)
+
+for i in "${targetList[@]}"; do
+	cross build --release --locked --target "$i"
+	zip -j "$NAME"-"$i" ./target/"$i"/release/dcompass*
+done
